e648e77a2e259683a1fb7cec868a7ad5de1dabf7
robincpc <chienpinchen@gmail.com>
2017-06-26 16:54:05 -0700

    Add a function to do color/gradient threshold filter.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def parse_arg(argv):
     parser = argparse.ArgumentParser(description='Advanced Lane Finding module')
     parser.add_argument('-c', '--calibrate', default=0, help='Set 1 if do calibration')
     parser.add_argument('-fd','--folder', default='./camera_cal/', help='the folder that consist images for calibration.' )
-    parser.add_argument('-f', '--file', defulat='./project_video.mp4', help='the file for finding lane lines.')
+    parser.add_argument('-f', '--file', default='./project_video.mp4', help='the file for finding lane lines.')
     return parser.parse_args(argv[1:])
 
 
@@ -91,9 +91,46 @@ def calib(fold='./camera_cal/', nx=9, ny=6):
 
 
 # Color/gradient threshold
+def thresh_pipeline(img, s_thresh=(140, 255), sx_thresh=(20, 100)):
+    '''
+    work on undistorted image
+    '''
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hsv[:,:,1]
+    s_channel = hsv[:,:,2]
+    # Sobel x
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
+    #sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # TODO: why use l_channel Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+
+    # Combine the two binary thresholds
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+    return combined_binary #color_binary
 
 
 # Perspective transform
+def warped():
+    '''
+    return Perspective matrix
+    Currently, The points for perspective are chosed manually.
+    '''
 
 
 # Detect lane lines
